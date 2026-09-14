8f011b34abefc69265aae5ab23b1bdf7aed5daf8
Izzet-Aghayev <agayevizzet@gmail.com>
2026-09-14 07:52:11 +0400

    fix(vercel): driver swapped to pymysql to solve build exit 1

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,9 @@
 import os
 
+import pymysql
+pymysql.version_info = (1, 4, 3, "final", 0)
+pymysql.install_as_MySQLdb()
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
